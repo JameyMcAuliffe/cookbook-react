0f20fa2382f5ed14b145b490134b9ff2c617e291
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-07-04 08:50:59 -0500

    signin functional

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { SignUpLink } from '../SignUp/SignUp';
+import { FirebaseContext } from '../Firebase/index';
+import * as ROUTES from '../../constants/routes';
 
 
 const SignIn = () => {
 	return (
-		<div>SignIn</div>
+		<div>
+			<h1>Sign In</h1>
+			<FirebaseContext.Consumer>
+				{firebase => <SignInFormWithRouter firebase={firebase}/>}
+			</FirebaseContext.Consumer>
+			<SignUpLink />
+		</div>
 	);
 }
 
+const INITIAL_STATE = {
+	email: '',
+	password: '',
+	error: null
+};
+
+const SignInForm = (props) => {
+
+	const [userState, setUserState] = useState({...INITIAL_STATE});
+
+	let onSubmit = (e) => {
+		const { email, password } = userState;
+
+		props.firebase
+			.doSignInWithEmailAndPassword(email, password)
+			.then(() => {
+				setUserState({...INITIAL_STATE});
+				props.history.push(ROUTES.HOME);
+			})
+			.catch(error => {
+				setUserState({...userState, error: error});
+			});
+
+		e.preventDefault();
+	}
+
+	let onChange = (e) => {
+		setUserState({...userState, [e.target.name]: e.target.value});
+	}
+
+	const {email, password, error} = userState;
+
+	const isInvalid = password.trim() === '' || email.trim() === '';
+
+	return (
+		<form onSubmit={onSubmit}>
+			<input
+				name="email"
+				value={email}
+				onChange={onChange}
+				type="text"
+				placeholder="Enter your email address"
+			/>
+			<input
+				name="password"
+				value={password}
+				onChange={onChange}
+				type="text"
+				placeholder="Enter your password"
+			/>
+			<button disabled={isInvalid} type="submit">Sign In</button>
+			{error && <p>{error.message}</p>}
+		</form>
+	);
+}
+
+//withRouter is a higher order component that gives access to React Router
+const SignInFormWithRouter = withRouter(SignInForm);
+
 export default SignIn;
+
+//j@gmail.com 123456
