e9b989aef2b6162f297bc31c6755b7cafb462ca2
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-10-07 12:57:55 -0400

    search component filters rendered recipes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,14 @@ import React, {useState, useEffect} from 'react';
 import { withAuthorization } from '../Session/index';
 import RecipeListing from './RecipeDisplay/RecipeListing/RecipeListing';
 import AddNewRecipeListing from './RecipeDisplay/RecipeListing/AddNewRecipeListing';
+import Search from './Search/Search';
 import './Home.css';
 
 const Home = (props) => {
 	const uid = props.firebase.auth.O;
 	const [recipesArray, setRecipesArray] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [renderedRecipes, setRenderedRecipes] = useState([]);
 
 	useEffect(() => {
 		//temporary array to hold returned data from fb
@@ -31,13 +34,24 @@ const Home = (props) => {
 		// eslint-disable-next-line
 	}, []);
 
-	//value to hold returned RecipeListing components for rendering below
-	let renderedRecipes = recipesArray.map((recipe, i) => {
-		return <RecipeListing image={recipe.image} title={recipe.title} key={recipe.recipeID} id={recipe.recipeID}/>
-	});
+	let onSearchUpdate = (e) => {
+		setSearchTerm(e.target.value.toLowerCase());
+	}
+
+	useEffect(() => {
+		let recipeComponents = recipesArray.filter((recipe) => {
+			return recipe.title.toLowerCase().includes(searchTerm);
+		})
+		.map((recipe, i) => {
+			return <RecipeListing image={recipe.image} title={recipe.title} key={recipe.recipeID} id={recipe.recipeID}/>
+		});
+
+		setRenderedRecipes(recipeComponents);
+	},[recipesArray, searchTerm]);
 	
 	return (
 		<div className="container-fluid home-div">
+			<Search onChange={onSearchUpdate} value={searchTerm}/>
 			<div className="row d-flex justify-content-around">
 				<AddNewRecipeListing />
 				{renderedRecipes}	
---
+++ src/components/Home/Search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Search.css';
+
+const Search = ({value, onChange}) => {
+	return (
+		<div className="mb-5 d-flex justify-content-center">
+			<input
+				onChange={onChange}
+				value={value} 
+				type="text"
+				placeholder="Search..."
+				className="bg-dark text-white search-bar mt-4 mb-4 col-sm-3 text-white"/>
+		</div>
+	);
+}
+
+export default Search;
