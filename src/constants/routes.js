bf6d36c4444d6fad6e5cae5ca96a58f65c96568a
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-10-15 14:08:32 -0400

    added ScrollToTop component

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,6 +4,4 @@ export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const RECIPE = '/recipe/:id';
 export const EDIT = '/recipe/:id/edit';
-export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
 export const NEW_RECIPE = '/new-recipe'
